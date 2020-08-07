4d23497c63b931f7a2eb6873d21117368f3b841c
Axel_Rasse <rasseax@gmail.com>
2020-08-07 22:18:49 +0400

    Mise en place de la partie 2

--- app.py
+++ app.py
@@ -15,48 +15,63 @@ import dash_html_components as html
 from dash.dependencies import Output, Input, State
 import dash_table
 
-## CONFIG
-# dash & plotly
+
+'''
+   ------------------------------------------------------------------------------------------- 
+                                            CONFIG
+   ------------------------------------------------------------------------------------------- 
+
+'''
+## PLOTLY
 import plotly.io as pio
 pio.templates.default = "plotly_white"
 config_dash = {'displayModeBar': False, 'showAxisDragHandles':False}
-margin = dict(l=0, r=0, t=0, b=0)
+no_margin = dict(l=0, r=0, t=0, b=0)
 # get acess to mapbox
 with open('mapbox_token.txt') as f:
     lines=[x.rstrip() for x in f]
 mapbox_access_token = lines[0]
-px.set_mapbox_access_token(mapbox_access_token)
-# color palette
+
+## DASH
+# external CSS + dash bootstrap components
+external_stylesheets=[dbc.themes.BOOTSTRAP, "assets/main.css"]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+
+## COLOR PALETTE
 colors_palette = {'Computers':'#264653',
            'Phone':'#2a9d8f',
            'Gears':'#e9c46a',
            'TV & Monitor':'#f4a261',
            'Washing Machine':'#e76f51'}
 str_to_int = {key: i for i, key in enumerate(colors_palette.keys())}
-# Readable big number
+blue_info_color = '#16a2b8'
+
+# READ BIG NUMBER
 def millify(n):
     if n>999:
         if n > 1e6-1:
             return f'{round(n/1e6,1)}M'
         return f'{round(n/1e3,1)}K'
     return n
-# external CSS + dash bootstrap components
-external_stylesheets=[dbc.themes.BOOTSTRAP, "assets/main.css"]
-# config dash
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
 
+'''
+   ------------------------------------------------------------------------------------------- 
+                                            CREATE THE FIGURE
+   ------------------------------------------------------------------------------------------- 
+
+'''
 ## LOAD DATA
 dirty_data = pd.read_csv('all_data.csv')
 data = pd.read_csv('clean_data.csv')
 
-cities = data['City'].unique()
+## 1. ANALYSE DES PRODUITS
+## -----------------------
 product_report = data.groupby(['Categories', 'Product','Price Each']).sum()
 product_list = product_report.index.get_level_values('Product')
 categories_list =product_report.index.get_level_values('Categories')
-
-
-## PARCAST  : découverte des produits
+# PARCAST  : découverte des produits
 df = product_report.reset_index().sort_values('Categories')
 cat_dim = go.parcats.Dimension(values=df['Categories'].values)
 product_dim = go.parcats.Dimension(values=product_list)
@@ -67,9 +82,9 @@ parcats = go.Figure(go.Parcats(
     line={'color':colors, 'colorscale':colorscale, 'shape':'hspline'},
     tickfont = dict(size = 12),
     hoverinfo='none'))
-parcats.update_layout(margin=dict(l=75, r=150, t=0, b=0))
+parcats.update_layout(margin=no_margin)
 
-## BAR CHART : Quel produit fait le meilleur chiffre de vente
+# BAR CHART : Quel produit fait le meilleur chiffre de vente
 product_rank = product_report['Sales'].sort_values().reset_index(['Categories','Price Each'])
 product_bar= go.Figure()
 top3 = product_rank['Sales'][-3:][::-1]
@@ -92,7 +107,7 @@ for product in product_rank.index:
 	    showlegend=legend_status,
 	    name=cat))
 product_bar.update_layout(
-	margin=dict(l=0, r=0, t=0, b=0),
+	margin=no_margin,
 	title_font_size=20,
 	hovermode = 'y unified',
 	hoverlabel=dict(bgcolor="white",font_size=12),
@@ -100,10 +115,10 @@ product_bar.update_layout(
 		orientation="v",
 		yanchor="bottom", y=0.5,
 		xanchor="right", x=0.93))
-product_bar.update_yaxes(showgrid=False, linecolor='black', linewidth=1.5)
+product_bar.update_yaxes(showgrid=False, linecolor='black', linewidth=0.5)
 product_bar.update_xaxes(nticks=5)
 
-## SCATTER PLOT: volume des ventes des produits selon leurs prix
+# SCATTER PLOT: volume des ventes des produits selon leurs prix
 df = product_report[['Sales', 'Quantity Ordered']].reset_index()
 size = df['Quantity Ordered']
 scatter_plot_product = go.Figure(go.Scatter(x=df["Price Each"], 
@@ -122,13 +137,11 @@ scatter_plot_product = go.Figure(go.Scatter(x=df["Price Each"],
                           ))
 scatter_plot_product.update_xaxes(title='<b>Prix des produits</b>, en $', nticks=5, zerolinewidth=1, zerolinecolor='black')
 scatter_plot_product.update_yaxes(title='<b>Volume de ventes</b>, en $', zerolinewidth=1, zerolinecolor='black')
-scatter_plot_product.update_layout(
-	margin = dict(t=0, l=0, r=0, b=0),  
-	)
+scatter_plot_product.update_layout(margin = no_margin)
 scatter_plot_product.add_annotation(x=1700, y=8e6, text='Macbook Pro', font_color='#264653', arrowcolor='#264653')
 scatter_plot_product.add_annotation(x=600, y=4e5, text='Machine à laver', font_color='#e76f51', arrowcolor='#e76f51')
 
-## DONUT: Analyse du volume de ventes
+# DONUT: Analyse du volume de ventes
 labels = categories_list
 colors  = product_report.reset_index(0)['Categories'].apply(lambda x: colors_palette[x])
 donut = go.Figure(go.Pie(labels=labels, values=product_report['Quantity Ordered'], marker_colors= colors,
@@ -140,11 +153,9 @@ donut = go.Figure(go.Pie(labels=labels, values=product_report['Quantity Ordered'
                          font=dict(size=25)),
                     ))
 donut.update_traces(hole=.4,textinfo='label+percent')
-donut.update_layout(
-    margin = dict(t=0, l=0, r=0, b=0),  
-    showlegend=False)
+donut.update_layout(margin = no_margin, showlegend=False)
 
-## BAR CHART: Quel est le nombre de produit different acheté par commande?
+# BAR CHART: Quel est le nombre de produit different acheté par commande?
 df = data.groupby(['Order ID', 'Order Date']).count()['Product'].value_counts()
 df = df.reset_index()
 df['percent'] = np.round(df['Product']/df['Product'].sum() * 100,2)
@@ -152,14 +163,82 @@ df.set_index('index', inplace=True)
 prod_by_order = go.Figure(go.Bar(
     y = df['Product'],
     x = df.index,
+  	marker=dict(color=blue_info_color),
     customdata = df['percent'],
     hovertemplate = '%{y} commandes<br> ↳ soit %{customdata} % des ventes<extra></extra>',
 ))
-prod_by_order.update_layout(hoverlabel=dict(bgcolor="white",font_size=14), hovermode='x', margin=dict(l=0, r=0, t=0, b=0))
+prod_by_order.update_layout(hoverlabel=dict(bgcolor="white",font_size=14), hovermode='x', margin=no_margin)
 prod_by_order.update_yaxes(showgrid=False,showticklabels=False)
 prod_by_order.update_xaxes(title = 'Nombre de produits achetés par commandes', showgrid=False)
 
-## DASH LAYOUT
+## 2. ANALYSE DES LIEUX DE VENTES
+## -------------------------------
+city_sales = data.groupby(['City','lat','long']).sum()['Sales'].reset_index()
+city_sales['Sales_text'] = city_sales['Sales'].apply(lambda x: millify(x))
+cities = city_sales['City']
+city_sales['percents'] = city_sales['Sales']/city_sales['Sales'].sum()
+
+# MAP: cartographie des lieux de ventes
+map_plot = go.Figure(go.Scattermapbox(
+        lat = city_sales['lat'], 
+        lon = city_sales['long'],
+        marker = dict(size=city_sales['Sales']/200000, opacity=0.5, color='#094D92'),
+        mode="markers+text",
+        textposition="top center",
+        textfont=dict(family="sans serif", size=14, color='black'),
+        hovertemplate ='<b>%{text}</b><br>' +
+                '%{customdata} $<extra></extra>',
+        text = cities,
+        customdata = city_sales['Sales_text']))
+                     
+map_plot.update_layout(
+	hoverlabel=dict(
+		bgcolor="white",
+		font_size=12),
+	margin = no_margin,
+	mapbox = dict(
+		accesstoken = mapbox_access_token,
+	   	zoom = 2.5,
+	   	center = go.layout.mapbox.Center(lat=40,lon=-97),
+	   	style = "mapbox://styles/axelitorosalito/ckb2erv2q148d1jnp7959xpz0"), 
+	showlegend=False
+)
+
+# BAR PLOT : San Francisco est la ville au plus gros volume de ventes
+df = city_sales.sort_values(by='Sales', ascending=False)
+best_city_plot= go.Figure(go.Bar(
+    x = df['City'], 
+    y = df['Sales'],
+    customdata=df['percents'],
+    hovertemplate ='<b>%{y:.3s} $</b><br>'+'soit %{customdata:%} du CA<br><extra></extra>'))
+best_city_plot.update_layout(
+	margin = no_margin,
+	hovermode = 'x unified', 
+	hoverlabel=dict(
+		bgcolor="white",
+		font_size=12))
+best_city_plot.update_yaxes(title='<b>Volume de ventes</b>, en $', nticks=5)
+best_city_plot.update_xaxes(showgrid=False, linecolor='black', linewidth=0.5)
+best_city_plot.update_traces(marker_color = blue_info_color)
+
+# PIE: San Francisco, New York et Los Angeles représent 53% des ventes
+pie_color = ['grey'] * len(city_sales)
+pie_color[:3] = [blue_info_color] * 3
+city_pie_plot =go.Figure(go.Pie(
+    labels=df['City'],
+    values=df['Sales'],
+))
+city_pie_plot.update_layout(margin=no_margin, showlegend=False)
+city_pie_plot.update_traces(
+    textposition='inside', 
+    textinfo='percent+label',
+    marker=dict(colors=pie_color, line=dict(color='#000000', width=0.2))
+)
+
+'''------------------------------------------------------------------------------------------- 
+                                            DASH LAYOUT
+   ------------------------------------------------------------------------------------------- 
+'''
 app.layout = dbc.Container([
 	html.Div(children=[
 		dcc.Markdown('''
@@ -228,6 +307,7 @@ app.layout = dbc.Container([
 		),
 
 	## 1. ANALYSE DES PRODUITS
+	## -----------------------
 		html.H1('1. ANALYSE DES PRODUITS') ,
 		html.Hr(),
 		dcc.Markdown('''
@@ -250,7 +330,6 @@ app.layout = dbc.Container([
 			Après avoir pris connaissances des differents produits, nous pouvons analyser les ventes pour __déterminer ceux qui se sont les mieux vendus en 2019__. 
 			En regardant le diagramme en barres ci-dessous on constate que le macbook pro represente  le plus gros chiffre de ventes.
 		''', className='mt-5'), 
-		# html.Hr(),
 		dbc.Row(dbc.Col(html.H1("Quels sont les produits phares?"), width={'offset':3}, className='mb-2')),
 		dbc.Row([
 			dbc.Col(
@@ -272,18 +351,18 @@ app.layout = dbc.Container([
 				width=3,
 			) for rank,(p, p_revenue) in enumerate(zip(top3.index, top3.values))]
 		],className="mb-2"),
-		html.H4("Classement des produits selon leur Chiffre d'Affaires"),
+		html.H4("Classement des produits selon leurs ventes ($)"),
 		dbc.Card(
 			dbc.CardBody(
 			[
 				dcc.Graph(figure=product_bar, config=config_dash),
 				dbc.Alert('''Ce diagramme liste dans l'ordre décroisant les produits selon leur importance pour le Chiffre d'Affaires. 
-							On attribue à chaque catégorie une couleur afin de distinguer la catégorie de chacun des produits''', 
-							color="light",
-							className="card-text"),
+					On attribue à chaque catégorie une couleur afin de distinguer la catégorie de chacun des produits''', 
+					color="light",
+					className="card-text"),
 			]),
 		),
-		 # Analyse détailées produit
+		# Analyse détailées produit
 		dbc.Alert(
 			[
 				dcc.Markdown('''Dans cette première partie d'approfondisement, __nous mettrons en place une stratègie produits__ construite 
@@ -319,7 +398,7 @@ app.layout = dbc.Container([
 									2. __Stopper la vente de machines à laver__. Cette catégorie rapporte peut à notre entreprise. Il s'agit de produits 
 									encombrant et lourd, leur frais de livraison est élevés. Il est preferable de focaliser nos efforts dans d'autres catégories.
 
-									3. __Continuez la vente d'accesoires__. Cette catégorie représentent 75% des produits vendus, voir figure X. D'après la figure X+1,
+									3. __Continuer la vente d'accesoires__. Cette catégorie représentent 75% des produits vendus, voir figure X. D'après la figure X+1,
 									96% des commandes ne contiennent qu'un seul produit. On peut en conclure que les accesoires attirent un grand nombre de clients 
 									sur notre site. __Une stratégie interresante serait de valoriser les produits haut de gamme afin d'insiter les clients à réaliser 
 									des achats multiples.__'''),
@@ -328,7 +407,7 @@ app.layout = dbc.Container([
 								dbc.Row(dbc.Col(dbc.Alert("Figure X: Nombre de ventes des differentes catégories", color="light", className="mt-0"), width={"offset": 4})),
 								dbc.Row(dbc.Col(html.H3("96% des commandes sont constitués d'un seul produit", className="mb-0"), width={"offset": 3})),
 		    	        		dcc.Graph(figure=prod_by_order, config=config_dash),		
-								dbc.Row(dbc.Col(dbc.Alert("Figure X+1: Quantité de commandes suivant le nombre d'accesoires", color="light"), 
+								dbc.Row(dbc.Col(dbc.Alert("Figure X+1: Quantité de commandes suivant le nombre de produits différents achetés", color="light"), 
 									width={"offset": 3})),		    	        								
 					    	],
 					       	body=True, className='border-0'),
@@ -338,14 +417,35 @@ app.layout = dbc.Container([
 			color="info",
 			className="mt-2"
 		),
-		html.H1('2. ANALYSE DES VENTES') ,
+	## 2. ANALYSE DES LIEUX DE VENTES
+	## ------------------------------
+		html.H1("2. ANALYSE DES LIEUX DE VENTES") ,
 		html.Hr(),
+		dcc.Markdown("""__Le service de livraison est disponible dans 9 villes Américaine__, on compte des villes tels que New York, Los Angeles ou
+		encore San Francisco..."""),
+		dbc.Row(dbc.Col(
+			[
+				dcc.Graph(figure=map_plot, config={**config_dash, **{'scrollZoom': False}}),
+				dbc.Alert("""Figure X+2: Volume de ventes de chaque ville durant l'année 2019""", color="light"),
+			],
+			width=dict(offset=3)
+		)),
+		dcc.Markdown("""A l'aide des figures ci-dessous, on observe que __San Francisco est la ville qui a réalisé le plus gros volume de ventes en 2019__. 
+			De plus, on estime le volume de ventes des 3 villes les plus prolifiques à plus de 50% du Chiffre d'Affaires de 2019"""),
+		dbc.Row(dbc.Col(html.H3("San Francisco est la ville au plus gros volume de ventes"), width={"offset": 3})),
+		dcc.Graph(figure=best_city_plot, config=config_dash),
+		dbc.Row(dbc.Col(html.H3("San Francisco, Los Angeles et New York représentent 53% du Chiffres d'Affaires"), width={"offset": 3})),
+		dcc.Graph(figure=city_pie_plot, config=config_dash),		
 	])
 ], 
 # fluid=True
 )
 
-## INTERACTIVITÉ
+
+'''------------------------------------------------------------------------------------------- 
+                                            INTERACTIVITÉ
+   ------------------------------------------------------------------------------------------- 
+'''
 # Présentation du bouton étude detailée
 @app.callback(
     Output("collapse_0", "is_open"),
@@ -357,7 +457,7 @@ def toggle_collapse_0(n, is_open):
         return not is_open
     return is_open
 
-# Bouton analyse detailée, analyse des produits
+# 1. Analyse des produits,bouton analyse detailée
 @app.callback(
     Output("collapse_1", "is_open"),
     [Input("collapse_button_1", "n_clicks")],
