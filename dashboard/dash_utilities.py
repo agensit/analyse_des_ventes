7ba060ec3440242fefb5f7ae7c97cf5e2e39c57b
Axel_Rasse <rasseax@gmail.com>
2020-08-28 13:42:32 +0400

    Dash utilities creation de la class container

--- dashboard/dash_utilities.py
+++ dashboard/dash_utilities.py
@@ -17,15 +17,34 @@ server = app.server
 
 
 '''------------------------------------------------------------------------------------------- 
-                                        Cards Class
+                                       >> CARD <<
    ------------------------------------------------------------------------------------------- 
 '''
 
 class Card:
-    """Créer un panel pour notre dashboard"""
+    """
+    >> INPUTS <<
+    ---------------------------------------------------------------------------------------------
+        * title: card title (str)
+        * tooltip: add a tolltip to describe the card in the top right corner (str, default = False)
+        * graph: plotly figure (plotly.graph_objs._figure.Figure)
+        * dash_config: dash configuration for the plotly figure (dict, default = {'displayModeBar': False, 'showAxisDragHandles':False})
+        * width: card's width (1<=int<=12, default = 12)
+        * height: card's height (str default = '100%')
+        * row_number: row location in the dashboard (int)
+        * order: card's order in the row (int)
+
+    >> OUTPUT <<
+    -------------------------------------------------------
+    create a card object which could be use in the dashboard
+"""
     def __init__(self, title, graph, tooltip=None, dash_config={'displayModeBar': False, 'showAxisDragHandles':False}):
         self.title = html.H2(title)
         self.graph = dcc.Graph(figure=graph, config=dash_config, style={"width":"100%"})
+        self.width = 12
+        self.height = "100%" # need to be remoove
+        self.row_number = None
+        self.order = None
 
         if tooltip :
             self.tooltip = self.create_tooltip(tooltip) 
@@ -40,6 +59,14 @@ class Card:
             ]
         )
 
+    def set_format(self, width=12, height="100%"):
+        self.width = width
+        self.height = height # will be remoove
+
+    def set_location(self, row_number, order):
+        self.row_number = row_number
+        self.order = order
+
     def create(self):
         top_row = [dbc.Col(self.title, width=4, className="ml-5")]
         if self.tooltip:
@@ -48,37 +75,57 @@ class Card:
         return html.Div(
             [
                 dbc.Row(top_row, justify="between"),
-                dbc.Row(self.graph, className="ml-3 mr-3", style={"background-color": "blue"})
+                dbc.Row(self.graph, className="ml-3 mr-3")
             ],
-            className="m-4 border",
+            className="my-4 mx-0 border",
             style={"background-color": "white"}
         )
 
 
 '''------------------------------------------------------------------------------------------- 
-                                         Class Container
+                                         >> Container <<
    ------------------------------------------------------------------------------------------- 
 '''         
 class Container:
-    """Créer la partie central de notre dashboard"""
-    def __init__(self, cards_info, margin=4, backgorund_color="white"):
-        self.cards_info = cards_info
+    """
+        >> INPUTS <<
+        ---------------------------------------------------------------------------------------------
+            * margin: set the margin between the cards (0<=int<=5, default=4)
+            * backgorund_color: color of the background ( str, default="white")
+
+        >> OUTPUT <<
+        -------------------------------------------------------
+        Create the panel where we organise all our cards. This is the core of our dashboard
+    """
+    def __init__(self, cards, margin=4, background_color="white"):
+        self.cards = cards
         self.margin = margin
-        self.backgorund_color = backgorund_color
+        self.background_color = background_color
 
-    def get_card_info(self):
-        for (card, n_row, order, width) in self.cards_info:
-            print(card.title.children.upper())
-            print("---------------")
-            print(f"row number {n_row}")
-            print(f"width: {width}")
-            print(f"margin: {self.margin}")
+    def info(self):
+        print("CONTAINER:")
+        print("---------------")
+        print(f"margin: {self.margin}")
+        print(f"background color: {self.background_color}")
+        for i,card in enumerate(self.cards):
             print("")
+            print(f"CARD #{i}")
+            print(f"\ttitle:{card.title.children}")
+            print(f"\t width: {card.width}")
+            print(f"\t height: {card.height}")
+            print(f"\t row's number: {card.row_number}")
+            print("\t order in row: {card.order}")
 
-    # def create(self):
-
-
-
+    def create_row(self, n, size):
+        cards = [card for card in self.cards if card.row_number == n]
+        row = dbc.Row(
+            children =[],
+            style={"height": size},
+            className="mx-2"
+            )
+        for card in cards:
+            row.children.append(dbc.Col(card.create(), width=card.width))
+        return row
 
 
 
@@ -92,14 +139,22 @@ if __name__ == '__main__':
     df = px.data.iris() # iris is a pandas DataFrame
     fig = px.scatter(df, x="sepal_width", y="sepal_length")
     fig.update_layout(margin=dict(l=20, r=20, t=10, b=10))
+
+    # create cards
     card1 = Card("Boos", fig, tooltip="test")
+    card1.set_location(row_number=1, order=1)
+    card1.set_format(width=10)
     card2 = Card("attention", fig, tooltip="test")
+    card2.set_location(row_number=1, order=2)
+    card2.set_format(width=2)
+    cards = [card1, card2]
 
-    cards = [[card1,1,1,5],[card2,1,3,10]]
+    # container
     container = Container(cards)
-    container.get_card_info()
+    # container.info()
+    row1 = container.create_row(1, '100%')
 
-    app.layout = html.Div([card1.create(), card2.create()])
+    app.layout = html.Div(row1)
     app.run_server(debug=True)
 
 
